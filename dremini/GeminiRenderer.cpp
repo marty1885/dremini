7b59192fbe675f2f456db16c9f86955f79c0f10a
marty1885 <marty188586@gmail.com>
2022-03-26 12:35:02 +0800

    Support gemtext extension

--- dremini/GeminiRenderer.cpp
+++ dremini/GeminiRenderer.cpp
@@ -6,7 +6,19 @@
 using namespace drogon;
 using namespace dremini;
 
-std::pair<std::string, std::string> dremini::render2Html(const std::string_view gmi_source)
+static bool isSingleCharRepeat(const std::string &str)
+{
+    if(str.empty())
+        return false;
+    char ch = str[0];
+    for(auto c : str) {
+        if(c != ch)
+            return false;
+    }
+    return true;
+}
+
+std::pair<std::string, std::string> dremini::render2Html(const std::string_view gmi_source, bool extended_mode)
 {
     std::string res;
     std::string title;
@@ -22,8 +34,44 @@ std::pair<std::string, std::string> dremini::render2Html(const std::string_view 
         if(node.type != "list" && last_is_list == true)
             res += "</ul>\n";
 
-        if(node.type == "text")
-            res += "<p>"+text+"</p>\n";
+        if(node.type == "text") {
+            if(extended_mode) {
+                // all "-" or "=" and is at least 3 chars => horizontal line
+                if(!text.empty() && (text[0] == '-' || text[0] == '=')
+                    && text.size() >= 3 && isSingleCharRepeat(text)) {
+
+                    res += "<hr>\n";
+                    continue;
+                }
+                // scan for `` pairs. If found, add the text in the pair as a code block
+                std::string current_text;
+                bool in_code_block = false;
+                res += "<p>";
+                for(auto c : text) {
+                    if(c == '`' && !in_code_block) {
+                        in_code_block = true;
+                        if(!current_text.empty()) {
+                            res += current_text;
+                            current_text.clear();
+                        }
+                    } else if(c == '`' && in_code_block) {
+                        in_code_block = false;
+                        if(!current_text.empty()) {
+                            res += "<code>" + current_text + "</code>";
+                            current_text.clear();
+                        }
+                    } else {
+                        current_text += c;
+                    }
+                }
+                if(in_code_block)
+                    res += "`" + current_text + "</p>";
+                else
+                    res += current_text + "</p>";
+            }
+            else
+                res += "<p>"+text+"</p>\n";
+        }
         else if(node.type == "heading1")
             res += "<h1>"+text+"</h1>\n";
         else if(node.type == "heading2")
@@ -39,7 +87,7 @@ std::pair<std::string, std::string> dremini::render2Html(const std::string_view 
             std::string meta = node.meta;
             // Quick and dirty parameter hack
             auto n = meta.find('?');
-            if(n != std::string::npos && meta.find("gemini://") != 0)
+            if(n != std::string::npos && meta.find("gemini://") != 0 && meta.find("http") != 0)
             {
                 std::string url = meta.substr(0, n);
                 std::string param = meta.substr(n+1);
@@ -48,7 +96,7 @@ std::pair<std::string, std::string> dremini::render2Html(const std::string_view 
             res += "<div class=\"link\"><a href=\""+meta+"\">"+text+"</a></div>\n";
         }
         else if(node.type == "preformatted_text")
-            res += "<pre>"+text+"</pre>\n";
+            res += "<pre><code>"+text+"</code></pre>\n";
 
         if(node.type == "list") {
             if(last_is_list == false)
--- dremini/GeminiRenderer.hpp
+++ dremini/GeminiRenderer.hpp
@@ -6,6 +6,19 @@
 namespace dremini
 {
 
-std::pair<std::string, std::string> render2Html(const std::string_view geminiSrc);
+/**
+ * @brief Render Gemini gemtext to HTML.
+ * @param gmi_source The Gemini source.
+ * @param extended_mode Whether to use extended mode.
+ * @return The rendered HTML.
+ * 
+ * Extended mode: Adds support for non gemini standard, Markdown like syntax in the following list
+ * - "-" or "=" and is at least 3 chars => horizontal line:
+ *    ---
+ *    ===
+ * - `` pairs treated as inline code block:
+ *   `code`
+ */
+std::pair<std::string, std::string> render2Html(const std::string_view gemini_src, bool extended_mode = false);
 
 }
