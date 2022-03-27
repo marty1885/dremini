93054386d726ba6fc39f575bef115acacf6b5718
marty1885 <marty188586@gmail.com>
2022-03-27 22:37:31 +0800

    HTML inline image and code

--- dremini/GeminiRenderer.cpp
+++ dremini/GeminiRenderer.cpp
@@ -18,6 +18,35 @@ static bool isSingleCharRepeat(const std::string &str)
     return true;
 }
 
+static std::string renderPlainText(const std::string &text)
+{
+    // scan for `` pairs. If found, add the text in the pair as a code block
+    std::string res;
+    std::string current_text;
+    bool in_code_block = false;
+    for(auto c : text) {
+        if(c == '`' && !in_code_block) {
+            in_code_block = true;
+            if(!current_text.empty()) {
+                res += current_text;
+                current_text.clear();
+            }
+        } else if(c == '`' && in_code_block) {
+            in_code_block = false;
+            if(!current_text.empty()) {
+                res += "<code>" + current_text + "</code>";
+                current_text.clear();
+            }
+        } else {
+            current_text += c;
+        }
+    }
+    if(in_code_block)
+        return res+"`"+current_text;
+    else
+        return res+current_text;
+}
+
 std::pair<std::string, std::string> dremini::render2Html(const std::string_view gmi_source, bool extended_mode)
 {
     std::string res;
@@ -43,31 +72,7 @@ std::pair<std::string, std::string> dremini::render2Html(const std::string_view 
                     res += "<hr>\n";
                     continue;
                 }
-                // scan for `` pairs. If found, add the text in the pair as a code block
-                std::string current_text;
-                bool in_code_block = false;
-                res += "<p>";
-                for(auto c : text) {
-                    if(c == '`' && !in_code_block) {
-                        in_code_block = true;
-                        if(!current_text.empty()) {
-                            res += current_text;
-                            current_text.clear();
-                        }
-                    } else if(c == '`' && in_code_block) {
-                        in_code_block = false;
-                        if(!current_text.empty()) {
-                            res += "<code>" + current_text + "</code>";
-                            current_text.clear();
-                        }
-                    } else {
-                        current_text += c;
-                    }
-                }
-                if(in_code_block)
-                    res += "`" + current_text + "</p>";
-                else
-                    res += current_text + "</p>";
+                res += "<p>"+renderPlainText(text)+"</p>\n";
             }
             else
                 res += "<p>"+text+"</p>\n";
@@ -85,15 +90,26 @@ std::pair<std::string, std::string> dremini::render2Html(const std::string_view 
             if(text.empty())
                 text = node.meta;
             std::string meta = node.meta;
+            // If link to image. We convert it to <img> tag
+            if(extended_mode) {
+                if(meta.rfind(".png") != std::string::npos || meta.rfind(".jpg") != std::string::npos) {
+                    const std::string& alt = text;
+                    res += "<img src=\"" + meta + "\" alt=\"" + alt + "\">";
+                    continue;
+                }
+            }
             // Quick and dirty parameter hack
             auto n = meta.find('?');
-            if(n != std::string::npos && meta.find("gemini://") != 0 && meta.find("http") != 0)
-            {
+            if(n != std::string::npos && meta.find("gemini://") != 0 && meta.find("http") != 0) {
                 std::string url = meta.substr(0, n);
                 std::string param = meta.substr(n+1);
                 meta = url + "?query=" + param;
             }
-            res += "<div class=\"link\"><a href=\""+meta+"\">"+text+"</a></div>\n";
+            // Open new tab if external link
+            // HACK: Port TLGS's URL parser and use that to determine if external link
+            bool is_external = meta.find("://") != 0;
+            std::string target = is_external ? "_blank" : "_self";
+            res += "<div class=\"link\"><a href=\""+meta+"\" target=\""+target+"\">"+text+"</a></div>\n";
         }
         else if(node.type == "preformatted_text")
             res += "<pre><code>"+text+"</code></pre>\n";
@@ -101,7 +117,7 @@ std::pair<std::string, std::string> dremini::render2Html(const std::string_view 
         if(node.type == "list") {
             if(last_is_list == false)
                 res += "<ul>\n";
-            res += "  <li>"+text+"</li>\n";
+            res += "  <li>"+(extended_mode ? renderPlainText(text) : text)+"</li>\n";
             last_is_list = true;
         }
         else {
